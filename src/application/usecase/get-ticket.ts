a3a2d295276ce9ad1bf666df2a07252037ea5c13
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-31 18:26:57 -0300

    feat: implement GetTicket use case for retrieving ticket details

---
+++ src/application/usecase/get-ticket.ts
@@ -0,0 +1,16 @@
+import { TicketRepository } from "../../infra/repository/ticket-repository.js"
+
+export class GetTicket {
+  constructor(readonly ticketRepository: TicketRepository) {}
+  async execute(ticketId: string): Promise<Output> {
+    const ticket = await this.ticketRepository.getTicket(ticketId)
+    return ticket
+  }
+}
+
+type Output = {
+  ticketId: string
+  eventId: string
+  email: string
+  ticketPriceInCents: number
+}
