239d45bbd49372e550c738b037cfa71b81f29449
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-01 19:06:48 -0300

    refactor: ensure TicketRepositoryDatabase to use DatabaseConnection

--- src/infra/repository/ticket-repository.ts
+++ src/infra/repository/ticket-repository.ts
@@ -1,31 +1,41 @@
 import { Ticket } from "../../domain/ticket.js"
-import { db } from "../database/db.js"
+import DatabaseConnection from "../database/database-connection.js"
 
 export interface TicketRepository {
   saveTicket(ticket: Ticket): Promise<void>
   getTicket(ticketId: string): Promise<Ticket>
+  updateTicketStatus(ticketId: string, status: string): Promise<void>
 }
+
 export class TicketRepositoryDatabase implements TicketRepository {
+  constructor(readonly connection: DatabaseConnection) {}
+
   async saveTicket(ticket: Ticket) {
-    await db.ticket.create({
-      data: {
-        ticket_id: ticket.ticketId,
-        event_id: ticket.eventId,
-        email: ticket.email,
-        ticket_price_in_cents: ticket.ticketPriceInCents,
-        status: ticket.status,
-        date: ticket.date,
-      },
-    })
+    await this.connection.query((prisma) =>
+      prisma.ticket.create({
+        data: {
+          ticket_id: ticket.ticketId,
+          event_id: ticket.eventId,
+          email: ticket.email.value,
+          ticket_price_in_cents: ticket.ticketPriceInCents,
+          status: ticket.getStatus(),
+          date: ticket.date,
+        },
+      })
+    )
   }
 
   async getTicket(ticketId: string) {
-    const ticket = await db.ticket.findUnique({
-      where: { ticket_id: ticketId },
-    })
+    const ticket = await this.connection.query((prisma) =>
+      prisma.ticket.findUnique({
+        where: { ticket_id: ticketId },
+      })
+    )
+
     if (!ticket) {
       throw new Error("Ticket not found")
     }
+
     return new Ticket(
       ticket.ticket_id,
       ticket.event_id,
@@ -33,6 +43,15 @@ export class TicketRepositoryDatabase implements TicketRepository {
       ticket.ticket_price_in_cents,
       ticket.status,
       ticket.date
+    )
+  }
+
+  async updateTicketStatus(ticketId: string, status: string) {
+    await this.connection.query((prisma) =>
+      prisma.ticket.update({
+        where: { ticket_id: ticketId },
+        data: { status },
+      })
     )
   }
 }
