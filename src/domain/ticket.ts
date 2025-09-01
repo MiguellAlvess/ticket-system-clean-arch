30040adb7bf7677a5da36a550a800029b3a7be7f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-01 19:02:41 -0300

    feat: refactor Ticket class to use Email value object for email handling

--- src/domain/ticket.ts
+++ src/domain/ticket.ts
@@ -1,14 +1,18 @@
 import crypto from "crypto"
+import Email from "./email.js"
 
 export class Ticket {
+  email: Email
   constructor(
     readonly ticketId: string,
     readonly eventId: string,
-    readonly email: string,
+    email: string,
     readonly ticketPriceInCents: number,
     private status: string,
     readonly date: Date
-  ) {}
+  ) {
+    this.email = new Email(email)
+  }
 
   static create(eventId: string, email: string, ticketPriceInCents: number) {
     const ticketId = crypto.randomUUID()
@@ -22,5 +26,17 @@ export class Ticket {
       status,
       date
     )
+  }
+
+  cancel() {
+    const today = new Date()
+    const diff = (today.getTime() - this.date.getTime()) / (1000 * 60 * 60)
+    if (diff > 24)
+      throw new Error("Cancellation can only be done within 24 hours")
+    this.status = "cancelled"
+  }
+
+  getStatus() {
+    return this.status
   }
 }
