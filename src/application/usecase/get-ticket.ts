fc047ec79ead69e2d4103f430e879a68b9026160
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-01 19:02:17 -0300

    feat: refactor GetTicket use case to use DatabaseConnection for querying

--- src/application/usecase/get-ticket.ts
+++ src/application/usecase/get-ticket.ts
@@ -1,10 +1,22 @@
-import { TicketRepository } from "../../infra/repository/ticket-repository.js"
+import DatabaseConnection from "../../infra/database/database-connection.js"
 
 export class GetTicket {
-  constructor(readonly ticketRepository: TicketRepository) {}
+  constructor(readonly connection: DatabaseConnection) {}
   async execute(ticketId: string): Promise<Output> {
-    const ticket = await this.ticketRepository.getTicket(ticketId)
-    return ticket
+    const output = await this.connection.query((prisma) =>
+      prisma.ticket.findUnique({
+        where: { ticket_id: ticketId },
+        include: { event: true },
+      })
+    )
+    if (!output) throw new Error("Ticket not found")
+    return {
+      ticketId: output.ticket_id,
+      eventId: output.event_id,
+      email: output.email,
+      ticketPriceInCents: output.ticket_price_in_cents,
+      eventDescription: output.event.description,
+    }
   }
 }
 
@@ -13,4 +25,5 @@ type Output = {
   eventId: string
   email: string
   ticketPriceInCents: number
+  eventDescription: string
 }
