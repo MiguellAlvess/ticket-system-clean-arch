c6bdfc15c3e1f2dedb3d36a4da7509df2d4b4a78
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-01 19:06:20 -0300

    refactor: ensure EventRepositoryDatabase to use DatabaseConnection

--- src/infra/repository/event-repository.ts
+++ src/infra/repository/event-repository.ts
@@ -1,17 +1,24 @@
-import { db } from "../database/db.js"
 import { Event } from "../../domain/event.js"
+import DatabaseConnection from "../database/database-connection.js"
 
 export interface EventRepository {
   getEvent(eventId: string): Promise<Event>
 }
+
 export class EventRepositoryDatabase implements EventRepository {
+  constructor(readonly connection: DatabaseConnection) {}
+
   async getEvent(eventId: string) {
-    const event = await db.event.findUnique({
-      where: { event_id: eventId },
-    })
+    const event = await this.connection.query((prisma) =>
+      prisma.event.findUnique({
+        where: { event_id: eventId },
+      })
+    )
+
     if (!event) {
       throw new Error("Event not found")
     }
+
     return new Event(
       event.event_id,
       event.description,
